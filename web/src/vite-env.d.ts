b7e15b3ac6045db6d8d85cb85da0cd6620ee53a8
Andrew <9568432+Jarvichi@users.noreply.github.com>
2026-03-22 08:06:06 +0000

    fix(rollbar): replace CDN snippet with npm package import (#390)

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -2,3 +2,11 @@
 /// <reference types="vite-plugin-pwa/client" />
 
 declare const __BUILD_DATE__: string
+
+interface ImportMetaEnv {
+  readonly VITE_GIT_SHA?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
